a35a5c4445d4707472608f1a4cee1f0f167d1f6d
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-01-31 21:16:30 +0530

    Completed Chain Query

--- MongoDB and Mongoose/myApp.js
+++ MongoDB and Mongoose/myApp.js
@@ -98,8 +98,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+    Person.find({favoriteFoods:foodToSearch})
+          .sort({name:0}) //sort by name
+          .limit(2) //limit by 2
+          .select({age:0}) //hiding the age
+          .exec((err,data)=>{
+              if (err) return console.error(err);
+            done(null , data);
+          });
 };
 
 /** **Well Done !!**
